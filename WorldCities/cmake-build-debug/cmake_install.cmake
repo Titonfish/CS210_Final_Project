efb61538424e3c96aa0af09a5b2659f8415cce56
Charlie Polonus <capolonus@gmail.com>
2025-04-19 11:37:10 -0500

    Basic File Searching

--- WorldCities/cmake-build-debug/cmake_install.cmake
+++ WorldCities/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Charlie P/Documents/# Senior Files/Advanced Data Structures and Algorithms/MidtermRepo/CS210_Midterm_Project/WorldCities
+# Install script for directory: C:/Users/Charlie P/Documents/# Senior Files/Advanced Data Structures and Algorithms/MidtermRepo/CS210_Final_Project/WorldCities
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -52,6 +52,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "C:/Users/Charlie P/Documents/# Senior Files/Advanced Data Structures and Algorithms/MidtermRepo/CS210_Midterm_Project/WorldCities/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/Charlie P/Documents/# Senior Files/Advanced Data Structures and Algorithms/MidtermRepo/CS210_Final_Project/WorldCities/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
--- WorldCities/main.cpp
+++ WorldCities/main.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
+#include <vector>
+#include "CSVReader.cpp"
+using namespace std;
+
+string GetFile();
+vector<vector<string>> LoadData(const string& filePath);
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    string filePath;
+    while (filePath == "")
+    {
+        filePath = GetFile();
+    }
+
+    CSVReader csv;
+    vector<vector<string>> fileData = csv.readCSV(filePath);
+
+    for (int i=1;i<=25;i++)
+    {
+        cout << fileData[i][0] << "," << fileData[i][1] << "," << fileData[i][2] << endl;
+    }
+}
+
+string GetFile()
+{
+    cout << "Input the name of the file containing all the world's cities: ";
+    string inputFileName;
+    getline(cin, inputFileName);
+
+    ifstream inFile(inputFileName.c_str());
+    if (!inFile) {
+        cout << "Error: Could not open input file.\n";
+        return "";
+    }
+
+    return inputFileName;
 }
