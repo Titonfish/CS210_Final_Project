d718416ae9f0500da5b5826b24e9b5fdc7328dd5
Charlie Polonus <capolonus@gmail.com>
2025-04-28 19:35:45 -0500

    Refactored FIFO implementation
    Added the ability to select what kind of cache to use, as well as renamed variables and methods to tailor the existing cache specifically to the FIFO cache implementation

--- WorldCities/main.cpp
+++ WorldCities/main.cpp
@@ -4,31 +4,59 @@
 #include <queue>
 using namespace std;
 
-string GetFile();
 vector<vector<string>> FilterByCountry(const vector<vector<string>>& allCities, const string& countryCode);
 vector<vector<string>> FilterByCity(const vector<vector<string>>& allCities, const string& cityName);
 string CityToString(const vector<string>& city);
-void UpdateCache(queue<vector<string>>& cache, const vector<string>& city);
-vector<vector<string>> SearchCache(queue<vector<string>>& cache, const string& countryCode, const string& cityName);
-void DisplayCache(queue<vector<string>>& cache);
+
+void FIFO_UpdateCache(queue<vector<string>>& cache, const vector<string>& city);
+vector<vector<string>> FIFO_SearchCache(queue<vector<string>>& cache, const string& countryCode, const string& cityName);
+void FIFO_DisplayCache(queue<vector<string>>& cache);
 
 int main()
 {
     string filePath;
-    while (filePath == "")
+    while (filePath.empty())
     {
-        filePath = GetFile();
+        cout << "Input the name of the file containing all the world's cities: ";
+        string inputFileName;
+        getline(cin, inputFileName);
+
+        ifstream inFile(inputFileName.c_str());
+        if (!inFile)
+        {
+            cout << "Error: Could not open input file." << endl;
+            continue;
+        }
+
+        filePath = inputFileName;
+    }
+
+    string cacheType;
+    while (cacheType.empty())
+    {
+        cout << "What kind of cache would you like to use? (lfu, fifo, rand) : ";
+        string cacheTypeInput;
+        getline(cin, cacheTypeInput);
+
+        if (cacheTypeInput == "lfu" || cacheTypeInput == "fifo" || cacheTypeInput == "rand")
+        {
+            cacheType = cacheTypeInput;
+        }
+        else
+        {
+            cout << "Error: undefined cache type" << endl;
+        }
     }
 
     cout << endl;
 
-    queue<vector<string>> cityCache;
-    CSVReader csv;
+    queue<vector<string>> fifo_cityCache;
+
     string userInput;
 
     while (true)
     {
-        cout << "Search for a city, or leave?\nEnter s to search, c to see the cache, or any other key to quit:" << endl;
+        cout << "Search for a city, or leave?" << endl << "Enter s to search, c to see the cache, or any other key to quit:" << endl;
         cin >> userInput;
         if (userInput != "s" && userInput != "S" && userInput != "c" && userInput != "C")
         {
@@ -36,7 +64,10 @@ int main()
         }
         if (userInput == "c" || userInput == "C")
         {
-            DisplayCache(cityCache);
+            if (cacheType == "fifo")
+            {
+                FIFO_DisplayCache(fifo_cityCache);
+            }
             cout << endl << endl;
             continue;
         }
@@ -49,25 +80,26 @@ int main()
         cout << "What is the name of the city you are searching for?" << endl;
         cin >> cityName;
 
-        vector<vector<string>> foundInCache = SearchCache(cityCache, countryCode, cityName);
-
-        if (foundInCache.empty())
+        if (cacheType == "fifo")
         {
-            cout << "No cities found in cache with that city name and country code" << endl;
-        }
-        else
-        {
-            cout << "Found " << foundInCache.size() << " from cache:" << endl;
-            for (vector<string> &curCity : foundInCache)
+            if (vector<vector<string>> foundInCache = FIFO_SearchCache(fifo_cityCache, countryCode, cityName); foundInCache.empty())
             {
-                cout << CityToString(curCity) << endl;
+                cout << "No cities found in cache with that city name and country code" << endl;
             }
-            cout << endl;
+            else
+            {
+                cout << "Found " << foundInCache.size() << " from cache:" << endl;
+                for (vector<string> &curCity : foundInCache)
+                {
+                    cout << CityToString(curCity) << endl;
+                }
+                cout << endl;
 
-            continue;
+                continue;
+            }
         }
 
-        vector<vector<string>> citiesFound= csv.readCSV(filePath);
+        vector<vector<string>> citiesFound= CSVReader::readCSV(filePath);
         citiesFound = FilterByCountry(citiesFound, countryCode);
         citiesFound = FilterByCity(citiesFound, cityName);
 
@@ -81,7 +113,11 @@ int main()
             for (vector<string> &curCity : citiesFound)
             {
                 cout << CityToString(curCity) << endl;
-                UpdateCache(cityCache, curCity);
+
+                if (cacheType == "fifo")
+                {
+                    FIFO_UpdateCache(fifo_cityCache, curCity);
+                }
             }
         }
 
@@ -89,20 +125,6 @@ int main()
     }
 }
 
-string GetFile()
-{
-    cout << "Input the name of the file containing all the world's cities: ";
-    string inputFileName;
-    getline(cin, inputFileName);
-
-    ifstream inFile(inputFileName.c_str());
-    if (!inFile) {
-        cout << "Error: Could not open input file.\n";
-        return "";
-    }
-
-    return inputFileName;
-}
 vector<vector<string>> FilterByCountry(const vector<vector<string>>& allCities, const string& countryCode)
 {
     vector<vector<string>> remainingCities;
@@ -133,7 +155,7 @@ string CityToString(const vector<string>& city)
 {
     return "City Name: " + city[1] + ", Country Code: " + city[0] + ", Population: " + city[2];
 }
-void UpdateCache(queue<vector<string>>& cache, const vector<string>& city)
+void FIFO_UpdateCache(queue<vector<string>>& cache, const vector<string>& city)
 {
     cache.push(city);
     if (cache.size() > 10)
@@ -141,28 +163,39 @@ void UpdateCache(queue<vector<string>>& cache, const vector<string>& city)
         cache.pop();
     }
 }
-vector<vector<string>> SearchCache(queue<vector<string>>& cache, const string& countryCode, const string& cityName)
+vector<vector<string>> FIFO_SearchCache(queue<vector<string>>& cache, const string& countryCode, const string& cityName)
 {
     vector<vector<string>> foundCities;
 
-    int size = cache.size();
+    const unsigned int size = cache.size();
     for (int i = 0; i < size; i++)
     {
+        bool isFoundCity = false;
         if (cache.front()[0] == countryCode && cache.front()[1] == cityName)
         {
             foundCities.push_back(cache.front());
+            isFoundCity = true;
         }
 
         vector<string> curCity = cache.front();
         cache.pop();
+
+        if (!isFoundCity)
+        {
+            cache.push(curCity);
+        }
+    }
+
+    for (const auto & curCity : foundCities)
+    {
         cache.push(curCity);
     }
 
     return foundCities;
 }
-void DisplayCache(queue<vector<string>>& cache)
+void FIFO_DisplayCache(queue<vector<string>>& cache)
 {
-    int size = cache.size();
+    const unsigned int size = cache.size();
     for (int i = 0; i < size; i++)
     {
         vector<string> curCity = cache.front();
